d4a90c8cc209856a58d1fda612b632150b408a9d
horang-e <rmsid1000@gmail.com>
2024-06-19 22:52:25 +0900

    [Add] 확장향 추가

--- src/components/blocks/ProfileBox/ProfileBox.tsx
+++ src/components/blocks/ProfileBox/ProfileBox.tsx
@@ -1,4 +1,5 @@
 import { IUser } from '@/components/@types/server';
+import Chips from '@/components/atoms/Chips/Chips';
 import React from 'react';
 
 type IProfileBoxProps = {
@@ -6,13 +7,31 @@ type IProfileBoxProps = {
   type: 'small' | 'full';
   //유저 정보
   user: IUser;
+  //유저 설명 비활성화 여부
+  disabledDescription?: boolean;
 };
 
 const ProfileBox = (props: IProfileBoxProps) => {
-  return (
+  return props.type === 'full' ? (
+    <div className='flex items-center flex-col gap-1 w-full'>
+      {!props.disabledDescription && (
+        <Chips
+          projectTag={{
+            CONTENT: props.user.JOB_TYPE,
+          }}
+        />
+      )}
+      <img src={props.user.PROFILE_PICTURE} alt='프로필 사진' className='w-[74px] h-[74px] rounded-full my-2' />
+      <p className='text-sm font-medium text-grey-300'>잔디행성까지 100m 남은</p>
+      <p className='text-xl font-semibold text-grey-400'>{props.user.NICK_NAME}</p>
+      {!props.disabledDescription && (
+        <p className='text-sm font-light text-grey-300 text-wrap mt-4  w-full line-clamp-2	'>{props.user.DESCRIPTION}</p>
+      )}
+    </div>
+  ) : (
     <div className='flex items-center gap-2'>
       <img src={props.user.PROFILE_PICTURE} alt='프로필 사진' className='w-[20px] h-[20px] rounded-xl' />
-      <p className='text-xs font-light'>{props.user.NICK_NAME}(19KM)</p>
+      <p className='text-xs font-medium text-grey-200'>{props.user.NICK_NAME}(19KM)</p>
     </div>
   );
 };
--- src/components/blocks/ProjectBox/ProjectBox.tsx
+++ src/components/blocks/ProjectBox/ProjectBox.tsx
@@ -18,46 +18,81 @@ interface IProjectBoxProps {
   // onClickLike: (projectId?: number, userId?: number, like?: boolean) => void;
   //프로젝트 생성 유저
   user: IUser;
+  //확장형, 축소형
+  type: 'small' | 'full';
 }
 
 const ProjectBox = (props: IProjectBoxProps) => {
+  const typeStyleConfig: any = {
+    outer: {
+      small: 'flex flex-col min-w-[390px] bg-white p-8 rounded-xl shadow-lg min-h-[292px] justify-between',
+      full: 'flex min-w-[800px] bg-white p-8 rounded-xl  min-h-[292px] justify-between border-grey-100 border-2 ',
+    },
+    inner: {
+      small: 'flex flex-col',
+      full: 'flex flex-col w-2/3 pr-8',
+    },
+    profile: {
+      small: 'flex gap-2 justify-between items-center',
+      full: 'flex justify-center gap-2 items-center w-1/3 border-l-2 border-grey-100 ',
+    },
+  };
+
   return (
-    <div className='flex flex-col min-w-[390px] bg-white p-8 rounded-xl shadow-lg min-h-[292px] justify-between'>
-      {/* 태그&좋아요 */}
-      <div className='flex flex-row justify-between mb-2'>
-        {/* 태그 */}
-        <div className='flex flex-row gap-2'>
-          {props.projectTags.map((tag, index) => (
-            <Chips key={index} projectTag={tag} />
+    <div className={typeStyleConfig.outer[props.type]}>
+      <div className={typeStyleConfig.inner[props.type]}>
+        {/* 태그&좋아요 */}
+        <div className='flex flex-row justify-between mb-2'>
+          {/* 태그 */}
+          <div className='flex flex-row gap-2'>
+            {props.projectTags.map((tag, index) => (
+              <Chips key={index} projectTag={tag} />
+            ))}
+          </div>
+          {/* 좋아요 */}
+          <img src='/heart_grey.svg' alt='좋아요' className='w-[20px] h-[20px]' />
+        </div>
+        {/* 내용 */}
+        <div className='min-h-[100px] '>
+          <p className='font-semibold text-xl mb-1'>{props.project.TITLE}</p>
+          <p className='font-light mb-2 text-grey-300'>{props.project.DESCRIPTION}</p>
+        </div>
+        {/* 프로젝트 태그 */}
+        <div className='flex gap-2 mb-2'>
+          {props.project.PROJECT_TAGS.map((tag, index) => (
+            <div className='text-xs text-primary'>#{tag}</div>
           ))}
         </div>
-        {/* 좋아요 */}
-        {/* <button onClick={() =>
-          //  props.onClickLike(props.project.PROJECT_ID, props.user.USER_ID, !props.userLike)
-          {}
-           }>
-          {props.projectLikes}
-        </button> */}
-        <div>조아여</div>
+        {/*full일때 뷰수, 좋아요수 */}
+        {props.type === 'full' && (
+          <div className='flex gap-2 justify-end items-center'>
+            <div className='flex gap-1 justify-between items-center'>
+              <img src='/heart_blue.svg' alt='좋아요' className='w-[14px] h-[12px]' />
+              <span className='text-xs text-grey-400'>{props.project.VIEW}</span>
+            </div>
+            <div className='flex gap-1 justify-between items-center'>
+              <img src='/eye_blue.svg' alt='좋아요' className='w-[15px] h-[10px]' />
+              <span className='text-xs text-grey-400'>{props.projectLikes}</span>
+            </div>
+          </div>
+        )}
       </div>
-      {/* 내용 */}
-      <div className='min-h-[100px] '>
-        <p className='font-semibold text-xl mb-1'>{props.project.TITLE}</p>
-        <p className='font-light mb-2'>{props.project.DESCRIPTION}</p>
-      </div>
-      {/* 프로젝트 태그 */}
-      <div className='flex gap-2 mb-2'>
-        {props.project.PROJECT_TAGS.map((tag, index) => (
-          <div className='text-xs'>#{tag}</div>
-        ))}
-      </div>
+
       {/* 유저프로필& 뷰수,좋아요수 */}
-      <div className='flex gap-2 justify-between'>
-        <ProfileBox type={'small'} user={props.user} />
-        <div>
-          <span className='text-xs'>{props.project.VIEW}</span>
-          <span className='text-xs'>{props.projectLikes}</span>
-        </div>
+      <div className={typeStyleConfig.profile[props.type]}>
+        <ProfileBox type={props.type} user={props.user} />
+        {props.type === 'small' && (
+          <div className='flex gap-2 justify-between items-center'>
+            <div className='flex gap-1 justify-between items-center'>
+              <img src='/heart_blue.svg' alt='좋아요' className='w-[14px] h-[12px]' />
+              <span className='text-xs text-grey-400'>{props.project.VIEW}</span>
+            </div>
+            <div className='flex gap-1 justify-between items-center'>
+              <img src='/eye_blue.svg' alt='좋아요' className='w-[15px] h-[10px]' />
+              <span className='text-xs text-grey-400'>{props.projectLikes}</span>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
